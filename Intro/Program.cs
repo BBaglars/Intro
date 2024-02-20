1e79c4942d5a90dc7e5d999160b04c204f3abacb
Berkay Bağlars <baglarsberkay38@gmail.com>
2024-02-20 10:54:45 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,38 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello World!");
+
+string message1 = "Krediler";
+
+int term = 12;
+
+double amount = 10.000;
+
+bool isAuthenicated = true;
+
+if (isAuthenicated)
+{
+    Console.WriteLine("Button-->Hoşgeldin Engin");
+}
+else
+{
+    Console.WriteLine("Button-->Sisteme Giriş Yap");
+}
+
+string[] loans = {"Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4" };
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+
+}
+
+Console.WriteLine("Kod Bitti");
